25d8fe842e4caf42fecf60b1bc4f79795088aea1
Sebastian Eschweiler <sebastian.eschweiler@gmail.com>
2018-07-26 13:52:03 +0200

    Part 2 Commit

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,46 @@
 import { Observable } from 'rxjs';
 
-var observable = Observable.create((observer: any) => {
-    observer.next('Hello World');
-    observer.next('Hello Again!');
-    observer.complete();
-    observer.next('Bye');
+var observable1 = Observable.create((observer: any) => {
+    observer.next('Observable One is alive!');
+    setInterval(() => {
+        observer.next('Observable One');
+    }, 5000);
 });
 
-observable.subscribe(
-    (x:any) => logItem(x),
-    (error: any) => logItem('Error: ' + error),
-    () => logItem('Completed')
+var observable2 = Observable.create((observer:any) => {
+    observer.next('Observable Two is alive!');
+    setInterval(() => {
+        observer.next('Observable Two');
+    }, 2500); 
+})
+
+var subscription1 = observable1.subscribe(
+    (x:any) => logItem(x, 1)
 );
 
-function logItem(val:any) {
+var subscription2 = observable2.subscribe(
+    (x:any) => logItem(x, 2)
+);
+
+document.getElementById('unsubscribeBtn1').addEventListener('click', () => {
+    subscription1.unsubscribe();
+});
+
+document.getElementById('unsubscribeBtn2').addEventListener('click', () => {
+    subscription2.unsubscribe();
+});
+
+document.getElementById('addSubscriptionBtn').addEventListener('click', () => {
+    subscription2.add(subscription1);
+});
+
+function logItem(val:any, column:any) {
     var node = document.createElement('li');
     var textnode = document.createTextNode(val);
     node.appendChild(textnode);
-    document.getElementById("list").appendChild(node);
+    if (column == 2) {
+        document.getElementById("list2").appendChild(node);  
+    } else if (column == 1) {
+        document.getElementById("list1").appendChild(node);
+    }
 }
